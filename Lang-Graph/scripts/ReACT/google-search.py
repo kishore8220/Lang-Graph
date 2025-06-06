3515af7b4de06cb0446066b2bdbc4f7d5de5d672
Kishore B <kdkishore91@gmail.com>
2025-06-06 17:53:55 +0530

    Update google-search.py

--- Lang-Graph/scripts/ReACT/google-search.py
+++ Lang-Graph/scripts/ReACT/google-search.py
@@ -1,7 +1,5 @@
 import os
 
-os.environ["GOOGLE_CSE_ID"] = "b0bdc34c178a4451d"
-os.environ["GOOGLE_API_KEY"] = "AIzaSyDV9wqt7P-JS8GI0mNCFne8BReLeUefuMU"
 
 from langchain_core.tools import Tool
 from langchain_google_community import GoogleSearchAPIWrapper
